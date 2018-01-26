9bfc9c3a94d20903e20b07a0a5443eaa6231cee8
Linh Le Duc <linh.le@mttjsc.com>
2018-01-26 20:53:42 +0700

    works save

---
+++ src/routes/subjects/index.js
@@ -0,0 +1,27 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import Subject from './Subject';
+import Layout from '../../components/Layout';
+import { checkAuth } from '../../utils/auth.valid.util';
+
+export default {
+  path: '/subjects',
+  action({ store }) {
+    const redirect = checkAuth(store);
+    if (redirect) return redirect;
+
+    return {
+      title: 'Danh mục môn học',
+      component: <Layout><Subject /></Layout>,
+    };
+  },
+
+};
