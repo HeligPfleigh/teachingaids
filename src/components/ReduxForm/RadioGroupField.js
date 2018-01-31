391122e92b1d370b5b4b9eb399e4e5cd4c0d76ff
Le Duc Linh <linh.le@mttjsc.com>
2018-01-31 18:19:54 +0700

    Update radio component of redux-form

--- src/components/ReduxForm/RadioGroupField.js
+++ src/components/ReduxForm/RadioGroupField.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { RadioButtonGroup } from 'material-ui/RadioButton';
+import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
 
-const renderRadioGroupField = ({ input, ...props }) => (
+const renderRadioGroupField = ({ options, styles, input, ...props }) => (
   <RadioButtonGroup
     {...input}
     {...props}
     valueSelected={input.value}
-    onChange={(event, value) => input.onChange(value)}
-  />
+  >
+    {
+      options.map((option, index) =>
+        <RadioButton
+          key={index}
+          style={styles}
+          value={option.value}
+          label={option.label}
+          onClick={() => {
+            input.onChange(option.value);
+          }}
+        />,
+      )
+    }
+  </RadioButtonGroup>
 );
 
 renderRadioGroupField.propTypes = {
   input: PropTypes.object.isRequired,
+  options: PropTypes.any.isRequired,
+  styles: PropTypes.any,
 };
 
 export default renderRadioGroupField;
