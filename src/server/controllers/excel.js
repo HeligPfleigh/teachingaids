3f2cae3a73c74afdb71783d8207f591328bcb572
Thanh D. Vu <thanhvd89@gmail.com>
2018-02-02 03:48:03 +0700

    Update import excel api

--- src/server/controllers/excel.js
+++ src/server/controllers/excel.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import _ from 'lodash';
 import EquipmentModel from '../../data/models/equipment';
 import EquipmentTypeModel from '../../data/models/equipment_type';
 import { generateBarcode } from '../../utils/barcode.util';
 import { getDataFromExcel } from '../../utils/excel.util';
+import removeToneVN from '../../utils/removeToneVN';
+import { UNITS } from '../../constants';
 
 const router = express.Router();
 
@@ -11,7 +14,7 @@ const getMaxSequenceNum = async () => {
   let maxSequenceNum = 0;
   if (allEquipment.length > 0) {
     maxSequenceNum = allEquipment
-      .map(value => value.sequenceNum)
+      .map(value => parseInt(value.sequenceNum, 10) || 0)
       .reduce((a, b) => Math.max(a, b));
   }
   return maxSequenceNum;
@@ -22,42 +25,43 @@ router.get('/excels', async (req, res) => {
   const sheetName = 'Tranh';
   const data = getDataFromExcel(pathFile, sheetName);
   const equipmentStatus = 'MOI';
-
-  let maxSequenceNum = await getMaxSequenceNum();
-
-  await data.map(async (item) => {
-    const equipmentTypeObj = {
-      name: item.name,
-      equipmentInfo: {
-        madeFrom: item.madeFrom,
-        grade: item.grade.split(',').map(g => parseInt(g, 10)),
-        khCode: item.khCode,
-      },
-      totalNumber: item.totalNumber,
-      subject: item.subject,
-      unit: item.unit,
-      order: item.order,
-    };
-    await EquipmentTypeModel.create(equipmentTypeObj, async (error, equipmentType) => {
-      if (error) {
-        console.log(`EquipmentType ERRORRRR!: ${item.name}`);
-      }
-      maxSequenceNum++;
-      const equipmentObj = {
-        barcode: generateBarcode(maxSequenceNum),
-        sequenceNum: maxSequenceNum,
-        equipmentTypeId: equipmentType.id,
-        status: equipmentStatus,
+  const units = _.invert(UNITS);
+  try {
+    let maxSequenceNum = await getMaxSequenceNum();
+    await data.map(async (item) => {
+      const equipmentTypeObj = {
+        name: item.name,
+        equipmentInfo: {
+          madeFrom: item.madeFrom,
+          grade: item.grade ? item.grade.split(',').map(g => parseInt(g, 10)) : null,
+          khCode: item.khCode,
+        },
+        totalNumber: item.totalNumber,
+        subject: item.subject,
+        unit: Object.prototype.hasOwnProperty.call(units, item.unit) ? units[item.unit] : _.toUpper(removeToneVN(item.unit)),
+        order: item.order,
       };
-      await EquipmentModel.create(equipmentObj, (err) => {
-        if (err) {
-          console.log(`Equipment ERRORRRR!: ${item.name}`);
+      await EquipmentTypeModel.create(equipmentTypeObj, async (error, equipmentType) => {
+        if (error) {
+          console.log(`EquipmentType ERRORRRR!: ${item.name} - ${error}`);
         }
+        maxSequenceNum++;
+        const equipmentObj = {
+          barcode: generateBarcode(maxSequenceNum),
+          sequenceNum: maxSequenceNum,
+          equipmentTypeId: equipmentType.id,
+          status: equipmentStatus,
+        };
+        await EquipmentModel.create(equipmentObj, (err) => {
+          if (err) {
+            console.log(`Equipment ERRORRRR!: ${item.name} - ${err}`);
+          }
+        });
       });
     });
-
-    return true;
-  });
+  } catch (e) {
+    console.log(e);
+  }
   // EquipmentModel.insertMany(arr, (error, docs) => {});
   // console.log(data);
   // workbook.SheetNames.forEach((n, i) => {
