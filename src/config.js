c2adb6ef314d8c92e69be3bdb84b094b252557a6
Le Duc Linh <linh.le@mttjsc.com>
2018-02-02 09:57:59 +0700

    connection db

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 export const port = process.env.PORT || 3000;
 export const host = process.env.WEBSITE_HOSTNAME || `localhost:${port}`;
 
-export const databaseUrl = process.env.DATABASE_URL || 'mongodb://localhost:27017/teachingaidsman';
+export const databaseUrl = process.env.DATABASE_URL || 'mongodb://localhost:27018/teachingaidsman';
 
 export const auth = {
   jwt: { secret: process.env.JWT_SECRET || 'my-secret-sns-administrator' },
